6f6bf8b520412a231478b59d3fae06e8bde0bf7c
isuruij <2020uelog@gmail.com>
2024-09-04 19:27:22 +0530

    updated

--- src/Component/user/MyAccount/Settings/UserAccount.jsx
+++ src/Component/user/MyAccount/Settings/UserAccount.jsx
@@ -1,87 +1,172 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Button from "react-bootstrap/Button";
-import Spinner from "react-bootstrap/Spinner";
+import { Button, message, Spin } from "antd"; // Import Ant Design components
 import { useNavigate } from "react-router-dom";
-import './UserAccount.css';
+import axios from "axios";
+import "./UpdateAccount.css";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
-function UserAccount() {
+function UpdateAccount() {
   const base_url = import.meta.env.VITE_APP_BACKEND_URL;
-  useEffect(() => {
-    console.log("Cookies:", document.cookie);
-  }, []);
+  const cookieValue = Cookies.get("token");
+  const userId = jwtDecode(cookieValue).id;
+  const id = userId;
+
+  const navigate = useNavigate();
   const [warning, setWarning] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [Password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [changePassword, setChangePassword] = useState(false); // State to manage password change option
   const [values, setValues] = useState({
     email: "",
-    password: "",
     name: "",
     address: "",
     mobileno: "",
-    officeno: "",
-    homeno: "",
     birthday: "",
-    agreeToConditions: "",
-    lakpawuraSource: "",
+    homeno: "",
+    officeno: "",
+    password:""
   });
 
-  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${base_url}/api/user/getuserbasicdetails/${userId}`
+        );
+        const { email, name, address, mobileno, birthday, officeno, homeno } =
+          response.data.Data;
+        setValues({
+          id,
+          email,
+          name,
+          address,
+          mobileno,
+          homeno,
+          birthday,
+          officeno,
+          password
+        });
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
 
-  //submitting PersonalDetails to backend
+    fetchData();
+  }, [userId]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (Password === "") {
-      setWarning("Enter password!");
-      return;
-    }
-    if (confirmPassword === "") {
-      setWarning("Confirm password!");
-      return;
+    if (changePassword) {
+      if (password === "") {
+        setWarning("Enter password!");
+        return;
+      }
+      if (confirmPassword === "") {
+        setWarning("Confirm password!");
+        return;
+      }
+      if (password !== confirmPassword) {
+        setWarning("Passwords do not match!");
+        setPassword("");
+        setConfirmPassword("");
+        return;
+      }
     }
 
-    if (Password !== confirmPassword) {
-      setWarning("Passwords do not match!");
-      setPassword("");
-      setConfirmPassword("");
-      setValues({ ...values, password: "" });
-      return;
-    }
+  
 
     try {
       setLoading(true);
-      const res = await axios.post(`${base_url}/api/user/register`, values);
-      console.log(res.data.message);
+      console.log(values);
+      const res = await axios.patch(
+        `${base_url}/api/user/updatebasicdetails`,
+        values
+      );
       if (res.data.Status === "Success") {
-        navigate("/projectsUser");
-      } else if (res.data.message === "already registered email") {
-        alert("Email is already registered! Please Enter another one");
+        message.success("Updated successfully!"); // Display success message
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000); // Navigate after a short delay
+      } else if (res.data.message === "user exist") {
+        message.error("Email is already registered!");
         setLoading(false);
       } else {
-        alert("System Error!");
+        message.error("System Error!");
         setLoading(false);
       }
-      console.log(res);
     } catch (error) {
-      console.log(error);
+      message.error("Error occurred during update!");
+      setLoading(false);
     }
   };
 
+
+  const handlePasswordChange = async (event) => {
+    event.preventDefault();
+    console.log("jjjjjjjjjjjjjjjjjjjjjj")
+
+    if (changePassword) {
+      if (password === "") {
+        setWarning("Enter password!");
+        return;
+      }
+      if (confirmPassword === "") {
+        setWarning("Confirm password!");
+        return;
+      }
+      if (password !== confirmPassword) {
+        setWarning("Passwords do not match!");
+        setPassword("");
+        setConfirmPassword("");
+        return;
+      }
+    }
+
+  
+
+    try {
+      setLoading(true);
+      console.log(values);
+      const res = await axios.patch(
+        `${base_url}/api/user/updatebasicdetailswithpassword`,
+        values
+      );
+      if (res.data.status) {
+        message.success("Updated successfully!"); // Display success message
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000); // Navigate after a short delay
+      } else if (res.data.message === "user exist") {
+        message.error("Email is already registered!");
+        setLoading(false);
+      } else {
+        message.error("System Error!");
+        setLoading(false);
+      }
+    } catch (error) {
+      message.error("Error occurred during update!");
+      setLoading(false);
+    }
+
+
+  };
+
   return (
-    <div className="form-groups">
-      <form onSubmit={handleSubmit} style={{marginTop:"400px"}}>
-        <h2>Update Account Details</h2>
+    <div style={{ marginLeft: "30vw", marginTop: "5vh" }}>
+      <form>
+        <h2>Update Account</h2>
 
         <div className="form-group">
-          <label className="lables">Email</label>
+          <label className="labels">Email</label>
           <div className="custom_input">
             <input
               required
               className="details-input form-control"
               type="email"
               id="email"
-              placeholder=""
+              value={values.email}
               onChange={(e) => {
                 setValues({ ...values, email: e.target.value });
               }}
@@ -90,14 +175,14 @@ function UserAccount() {
         </div>
 
         <div className="form-group">
-          <label className="lables">Name</label>
+          <label className="labels">Name</label>
           <div className="custom_input">
             <input
               required
               className="details-input form-control"
               type="text"
               id="name"
-              placeholder=""
+              value={values.name}
               onChange={(e) => {
                 setValues({ ...values, name: e.target.value });
               }}
@@ -106,30 +191,28 @@ function UserAccount() {
         </div>
 
         <div className="form-group">
-          <label className="lables">Permanent Address</label>
+          <label className="labels">Permanent Address</label>
           <div className="custom_input">
             <input
               className="details-input form-control"
               type="text"
               id="address"
-              placeholder=""
+              value={values.address}
               onChange={(e) => {
                 setValues({ ...values, address: e.target.value });
               }}
             />
           </div>
         </div>
-        <br></br>
-        <label className="lables">Contact Numbers</label>
-        <br></br>
-        <div className="form-group contact-1">
-          <label className="lables">Mobile</label>
+
+        <div className="form-group">
+          <label className="labels">Mobile No</label>
           <div className="custom_input">
             <input
               className="details-input form-control"
               type="text"
               id="mobileno"
-              placeholder=""
+              value={values.mobileno}
               onChange={(e) => {
                 setValues({ ...values, mobileno: e.target.value });
               }}
@@ -137,14 +220,14 @@ function UserAccount() {
           </div>
         </div>
 
-        <div className="form-group contact-1">
-          <label className="lables">Office </label>
+        <div className="form-group">
+          <label className="labels">Office No</label>
           <div className="custom_input">
             <input
               className="details-input form-control"
               type="text"
-              id="officeno"
-              placeholder=""
+              id="mobileno"
+              value={values.officeno}
               onChange={(e) => {
                 setValues({ ...values, officeno: e.target.value });
               }}
@@ -152,14 +235,14 @@ function UserAccount() {
           </div>
         </div>
 
-        <div className="form-group contact-1">
-          <label className="lables">Home</label>
+        <div className="form-group">
+          <label className="labels">Home No</label>
           <div className="custom_input">
             <input
               className="details-input form-control"
               type="text"
-              id="homeno"
-              placeholder=""
+              id="mobileno"
+              value={values.homeno}
               onChange={(e) => {
                 setValues({ ...values, homeno: e.target.value });
               }}
@@ -168,13 +251,13 @@ function UserAccount() {
         </div>
 
         <div className="form-group">
-          <label className="lables">Date of birth</label>
+          <label className="labels">Date of Birth</label>
           <div className="custom_input">
             <input
               className="details-input form-control"
               type="date"
               id="birthday"
-              placeholder=""
+              value={values.birthday}
               onChange={(e) => {
                 setValues({ ...values, birthday: e.target.value });
               }}
@@ -183,191 +266,71 @@ function UserAccount() {
         </div>
 
         <div className="form-group">
-          <label className="lables">New Password</label>
-          <div className="custom_input">
+          <label>
             <input
-              className="details-input form-control"
-              type="password"
-              id="password"
-              value={Password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setValues({ ...values, password: e.target.value });
-              }}
-            />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label className="lables">Confirm Password</label>
-          <div className="custom_input">
-            <input
-              className="details-input form-control"
-              type="password"
-              id="password2"
-              value={confirmPassword}
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
-            />
-          </div>
-          {warning && <p style={{ color: "red" }}>{warning}</p>}
-        </div>
-
-        <label className="lables">How do you know Lakpawura</label>
-        <br></br>
-        <br></br>
-
-        <div className="form-check">
-          <input
-            type="radio"
-            id="friend"
-            name="lakpawuraSource"
-            value="friend"
-            className=" form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, lakpawuraSource: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">
-            Introduced by a Friend
+              type="checkbox"
+              checked={changePassword}
+              onChange={() => setChangePassword(!changePassword)}
+            />{" "}
+            Change Password
           </label>
         </div>
 
-        <div className="form-check">
-          <input
-            type="radio"
-            id="family"
-            name="lakpawuraSource"
-            value="family"
-            className="form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, lakpawuraSource: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">
-            Introduced by a Family Member
-          </label>
-        </div>
+        {changePassword && (
+          <>
+            <div className="form-group">
+              <label className="labels">New Password</label>
+              <div className="custom_input">
+                <input
+                  className="details-input form-control"
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    setValues({ ...values, password: e.target.value });
+                  }}
+                />
+              </div>
+            </div>
 
-        <div className="form-check">
-          <input
-            type="radio"
-            id="company"
-            name="lakpawuraSource"
-            value="company"
-            className="form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, lakpawuraSource: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">
-            Introduced by the Company
-          </label>
-        </div>
-
-        <div className="form-check">
-          <input
-            type="radio"
-            id="socialMedia"
-            name="lakpawuraSource"
-            value="socialmedia"
-            className="form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, lakpawuraSource: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">Social Media</label>
-        </div>
-
-        <div className="form-check">
-          <input
-            type="radio"
-            id="dprWebsite"
-            name="lakpawuraSource"
-            value="dprWebsite"
-            className="form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, lakpawuraSource: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">Lakpawura Website</label>
-        </div>
-
-        <div className="form-check">
-          <input
-            type="radio"
-            id="other"
-            name="lakpawuraSource"
-            className="form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, lakpawuraSource: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">Other</label>
-        </div>
-
-        <label className="form-check-label lables">
-          Are you agree with conditions
-        </label>
-        <div className="form-check">
-          <input
-            type="radio"
-            id="agree"
-            name="Conditions"
-            value="yes"
-            className="form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, agreeToConditions: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">Yes</label>
-        </div>
-
-        <div className="form-check">
-          <input
-            type="radio"
-            id="disagree"
-            name="Conditions"
-            value="no"
-            className="form-check-input"
-            onChange={(e) =>
-              setValues({ ...values, agreeToConditions: e.target.value })
-            }
-          />
-          <label className="form-check-label lables">No</label>
-        </div>
+            <div className="form-group">
+              <label className="labels">Confirm Password</label>
+              <div className="custom_input">
+                <input
+                  className="details-input form-control"
+                  type="password"
+                  id="password2"
+                  value={confirmPassword}
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                  }}
+                />
+              </div>
+              {warning && <p style={{ color: "red" }}>{warning}</p>}
+            </div>
+          </>
+        )}
 
         <div style={{ display: "flex" }}>
           <Button
-            type="submit"
-            className="signup-Button button-color"
-            disabled={loading || values.agreeToConditions !== "yes"}
+            type="primary"
+            htmlType="submit"
+            loading={loading}
+            disabled={loading}
+            onClick={changePassword ? handlePasswordChange : handleSubmit}
           >
-            {loading ? (
-              <>
-                <Spinner
-                  style={{ marginLeft: "11px", marginRight: "11px" }}
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                  aria-hidden="true"
-                />
-              </>
-            ) : (
-              "Continue"
-            )}
+            {loading ? <Spin size="small" /> : "Update"}
           </Button>
         </div>
 
-        <br></br>
-        <br></br>
+        <br />
+        <br />
       </form>
-      <br></br>
-      <br></br>
+      <br />
+      <br />
     </div>
   );
 }
 
-export default UserAccount;
+export default UpdateAccount;
